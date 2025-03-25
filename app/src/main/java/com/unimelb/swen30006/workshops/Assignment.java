1587d7bd0535fd0826c44fffa8ed904ae173847d
Lionel <152048641+milckque@users.noreply.github.com>
2025-03-25 17:13:37 +1100

    added subject and assignment classes

---
+++ app/src/main/java/com/unimelb/swen30006/workshops/Assignment.java
@@ -0,0 +1,13 @@
+package com.unimelb.swen30006.workshops;
+
+import java.util.Date;
+
+public class Assignment {
+    private String description;
+    private Date dueDate;
+    private int maxAttempts;
+    private String name;
+
+    public Submission[] invalidSubmissions() {}
+    public Submission[] validSubmissions() {}
+}
