df832e90acec78ce47c79f6b64325e4fdee8d81c
Lionel <152048641+milckque@users.noreply.github.com>
2025-03-25 17:06:42 +1100

    made person student & staff classes

---
+++ app/src/main/java/com/unimelb/swen30006/workshops/Student.java
@@ -0,0 +1,9 @@
+package com.unimelb.swen30006.workshops;
+
+public class Student extends Person {
+    private String email;
+    private String studentID;
+
+    public void sendEmailMsg (String) {}
+    public float totalGrade() {}
+}
