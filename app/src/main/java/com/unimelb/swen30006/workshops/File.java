de62b922d94f0d7d889fec734df6e500c147fdbb
Lionel <152048641+milckque@users.noreply.github.com>
2025-03-25 17:10:17 +1100

    update pre-existings classes

--- app/src/main/java/com/unimelb/swen30006/workshops/File.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/File.java
@@ -1,7 +1,13 @@
 package com.unimelb.swen30006.workshops;
 
+import java.util.Date;
+
 // Sample private file class to be replaced by your implementaiton
 class File {
+    private Date createdDate;
+    private String fileData;
+    private String fileName;
+
     public String fileType(){
         double num = Math.random();
         if(num<0.5){
@@ -10,4 +16,8 @@ class File {
             return "docx";
         }
     }
+
+    public int fileSize(){}
+    public String content(){}
+
 }
